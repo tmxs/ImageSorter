4a2c529af1d39ef90a615b0e56c805b5c0d6e90b
tmxs <tmxs>
2020-12-04 18:54:44 +0100

    Refactor install script

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-cd /tmp
-curl -LO https://raw.githubusercontent.com/tmxs/ImageSorter/master/ImageSorter.py
-mv /tmp/ImageSorter.py ~/.local/bin/imagesorter
+curl https://raw.githubusercontent.com/tmxs/ImageSorter/master/ImageSorter.py > ~/.local/bin/imagesorter
 chmod +x ~/.local/bin/imagesorter
